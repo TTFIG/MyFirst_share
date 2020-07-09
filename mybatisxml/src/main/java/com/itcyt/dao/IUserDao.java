9c2b896956b4015eb3404229f0c9c5568d0d11f8
上扬 <1185828418@qq.com>
2020-07-10 00:06:35 +0800

    第一次提交

---
+++ mybatisxml/src/main/java/com/itcyt/dao/IUserDao.java
@@ -0,0 +1,31 @@
+package com.itcyt.dao;
+
+import com.itcyt.domain.User;
+
+import java.util.List;
+
+/**
+ * 用户的持久层接口
+ */
+public interface IUserDao {
+
+    /**
+     * 查询所有用户，同时获取到用户下所有账户的信息
+     * @return
+     */
+    List<User> findAll();
+
+
+    /**
+     * 根据id查询用户信息
+     * @param userId
+     * @return
+     */
+    User findById(Integer userId);
+
+    /**
+     * 更新用户信息
+     * @param user
+     */
+    void updateUser(User user);
+}
